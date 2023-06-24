022b5f85a5c36f9c2dc583a3103a9edc24b4f378
dmorgun <daryamorgun@gmail.com>
2023-06-24 07:40:47 -0500

    Finished Test1:Perform and verify sorting on the Inventory page

--- test/pages/Inventory.page.js
+++ test/pages/Inventory.page.js
@@ -9,9 +9,81 @@ class InventoryPage extends BaseSwagLabPage {
 
     get addItemToCartBtns() { return $$('[id^="add-to-cart"]'); }
 
+    // my selectors below
+    //get inventoryItemNames () {return $$('.inventory_item_name');}
+    //get inventoryItemPrices () {return $$('.inventory_item_price');}
+
+    // my getters below
+    get sortingDropdown() { return $('.product_sort_container'); }
+
+    get sortNameAtoZDropdownOption () { return $('option[value="az"]'); }
+
+    get sortNameZtoADropdownOption () { return $('option[value="za"]'); }
+
+    get sortPriceLowToHighDropdownOption () { return $('option[value="lohi"]'); }
+
+    get sortPriceHighToLowDropdownOption () { return $('option[value="hilo"]'); }
+
+
     async addItemToCartById(id) {
         await this.addItemToCartBtns[id].click();
     }
+    
+    //my methods below
+    async sortNameAtoZ() {
+        await this.sortingDropdown.click();
+        //await $('.product_sort_container').click();
+        await this.sortNameAtoZDropdownOption.click();
+        ////await $('option[value="az"]').click();
+    }
+
+    async sortNameZtoA() {
+        await this.sortingDropdown.click();
+        await this.sortNameZtoADropdownOption.click();
+    }
+
+    async sortPriceLowToHigh() {
+        await this.sortingDropdown.click();
+        await this.sortPriceLowToHighDropdownOption.click();
+    }
+
+    async sortPriceHighToLow() {
+        await this.sortingDropdown.click();
+        await this.sortPriceHighToLowDropdownOption.click();
+    }
+
+    async getItemNames() {
+        const items = await this.inventoryItems;
+        const itemNames = [];
+
+        for (let item of items) {
+            const itemText = await item.getText();
+            //transform long text to just element short name
+            const parts = itemText.split('\n');
+            const itemShortName = parts[0]
+            itemNames.push(itemShortName);
+        }
+        return itemNames;
+    }
+
+    async getItemPrices() {
+        const items = await this.inventoryItems;
+        const itemPrices = [];
+        
+        for (let item of items) {
+            const itemText = await item.getText();
+            //transform long text to just element short name
+            const parts = itemText.split('\n');
+            const itemPrice = parts[2]
+            itemPrices.push(itemPrice);
+        }
+
+        // I am not sure how this code below works
+        const itemPricesAsNumbers = itemPrices.map((price) => parseFloat(price.replace('$', '')));
+        return itemPricesAsNumbers;
+
+    }
+
 }
 
 module.exports = { InventoryPage };
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -24,4 +24,32 @@ describe('My First Test', () => {
         await pages.shopingCartPage.removeCartItemById(0);
         await expect(pages.shopingCartPage.cartItems).not.toBeExisting();
     });
+
+    it.only('Perform and verify sorting on the Inventory page', async () => {
+        await pages.loginPage.navigate();
+        await pages.loginPage.performLogin('standard_user', 'secret_sauce');
+
+        await pages.inventoryPage.sortNameAtoZ();
+        //retrieve all items names from the page and compare with sorted items array
+        const sortedItemsAZ = (await pages.inventoryPage.getItemNames()).sort();
+        expect (await pages.inventoryPage.getItemNames()).toEqual(sortedItemsAZ);
+        
+
+        await pages.inventoryPage.sortNameZtoA();
+        const unsortedItems = (await pages.inventoryPage.getItemNames());
+        // I don't know how this localCompare thing works
+        const sortedItemsZA = unsortedItems.sort((a, b) => b.localeCompare(a));
+        expect (await pages.inventoryPage.getItemNames()).toEqual(sortedItemsZA);
+
+        await pages.inventoryPage.sortPriceLowToHigh();
+        const unsortedPrices = (await pages.inventoryPage.getItemPrices());
+        const sortedPricesLoHi = unsortedPrices.sort((a, b) => a - b);
+        expect (await pages.inventoryPage.getItemPrices()).toEqual(sortedPricesLoHi);
+
+        await pages.inventoryPage.sortPriceHighToLow();
+        const sortedPricesHiLo = unsortedPrices.sort((a, b) => b - a);
+        expect (await pages.inventoryPage.getItemPrices()).toEqual(sortedPricesHiLo);
+        
+    });
+
 });
